a46c31e87fdbe7503cbbc9ecf32b76fbf030223f
maSZiej <maciej.szuberla@wp.pl>
2023-02-21 00:27:57 +0100

    dodanie buta

--- sklep_int/uzytkownicy/admin.py
+++ sklep_int/uzytkownicy/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Buty
 
 # Register your models here.
+admin.site.register(Buty)
--- sklep_int/uzytkownicy/models.py
+++ sklep_int/uzytkownicy/models.py
@@ -3,20 +3,22 @@ from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
-class Przedmiot(models.Model):
+class Buty(models.Model):
     Nazwa = models.CharField(max_length=255)
+    Wyglad = models.ImageField(upload_to='static')
     opis = models.TextField()
 
-    class Buty(models.IntegerChoices):
+    class Rozmiar(models.IntegerChoices):
         male1 = 38
         male2 = 39
-        srednie1 = 40
+        srednie1 = 40 
         srednie2 = 41
-        duze1 = 42
+        duze1 = 42 
         duze2 = 43
-    wybor = models.IntegerField(choices=Buty.choices)
+    wybor = models.IntegerField(choices=Rozmiar.choices,default=Rozmiar.srednie1)
+
+    
 
 class Ocena(models.Model):
-    przedmiot = models.ForeignKey(Przedmiot, on_delete=models.CASCADE)
+    przedmiot = models.ForeignKey(Buty, on_delete=models.CASCADE)
     komentarz=models.TextField()
-    
