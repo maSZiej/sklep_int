b8f73149cfc91e4e9c56d89e34314c4e8318b872
maSZiej <maciej.szuberla@wp.pl>
2023-02-21 21:34:26 +0100

    2134

--- sklep_int/uzytkownicy/serializer.py
+++ sklep_int/uzytkownicy/serializer.py
@@ -1,3 +1,14 @@
 from .models import Buty,Ocena
 from rest_framework import serializers
 
+class ButySerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        model = Buty
+        fields = ['Nazwa','Marka','Wyglad','opis']
+
+class OcenaSerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        model = Ocena
+        fields = ['przedmiot','komentarz']
